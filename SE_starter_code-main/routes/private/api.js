e8f4ae15db3de14ee606754cab007f0cafc7d87b
Zaid Rass <zaidqarxoy@gmail.com>
2023-05-23 12:56:16 +0300

    not complete deleet stations

--- SE_starter_code-main/routes/private/api.js
+++ SE_starter_code-main/routes/private/api.js
@@ -99,8 +99,10 @@ module.exports = function (app) {
   app.delete("/api/v1/station/:stationId", async(req,res)=>{
     try{
       const {StationId} =req.params;
-      
+      const selectedStation = db.select('*').from ("se_project.station").where("id",StationID);
+      if(selectedStation.)
       const deletedStation = await db ("se_project.stations").where("id",StationId).del().returning('*');
+
       console.log("Deleted", deletedStation);
       return res.status(200).json(deletedStation);
   
